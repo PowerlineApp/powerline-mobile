8b630f4bc1e65dc666f51dd46a50052709e0464a
Superstar <superstar@Superstars-Mac.local>
2015-10-09 05:18:15 -0700

    add unstaged files

---
+++ hooks/after_prepare/011_copy_files.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var path = require('path');
+
+var filestocopy = [{
+  "resources/notification_icon.png": "platforms/android/res/drawable-xhdpi/notification_icon.png",
+  "frameworks/FlurryAnalytics-4.1.0.jar": "platforms/android/libs/FlurryAnalytics-4.1.0.jar"
+}];
+
+var rootdir = process.argv[2];
+
+filestocopy.forEach(function (obj) {
+  Object.keys(obj).forEach(function (key) {
+    var val = obj[key];
+    var srcfile = path.join(rootdir, key);
+    var destfile = path.join(rootdir, val);
+    var destdir = path.dirname(destfile);
+    if (fs.existsSync(srcfile) && fs.existsSync(destdir)) {
+      fs.createReadStream(srcfile).pipe(
+              fs.createWriteStream(destfile));
+      console.log("[✓] Copied " + key + " to " + val);
+    }
+  });
+});
