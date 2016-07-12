403fabf31e7387cc27491c48ac40fd9f29f72f61
Peter Vojtek <peter.vojtek@gmail.com>
2016-07-12 12:24:24 +0200

    update cordova plugins: replace cordova-plugin-facebook4 repo due to #67

--- plugins/cordova-plugin-facebook4/src/ios/FacebookConnectPlugin.m
+++ plugins/cordova-plugin-facebook4/src/ios/FacebookConnectPlugin.m
@@ -6,6 +6,7 @@
 //  Updated by Mathijs de Bruin on 11-08-25.
 //  Updated by Christine Abernathy on 13-01-22
 //  Updated by Jeduan Cornejo on 15-07-04
+//  Updated by Eds Keizer on 16-06-13
 //  Copyright 2011 Nitobi, Mathijs de Bruin. All rights reserved.
 //
 
@@ -45,7 +46,7 @@
         //launchOptions is nil when not start because of notification or url open
         launchOptions = [NSDictionary dictionary];
     }
-    
+
     [[FBSDKApplicationDelegate sharedInstance] application:[UIApplication sharedApplication] didFinishLaunchingWithOptions:launchOptions];
 }
 
@@ -140,7 +141,7 @@
     if ([command.arguments count] > 0) {
         permissions = command.arguments;
     }
-    
+
     // this will prevent from being unable to login after updating plugin or changing permissions
     // without refreshing there will be a cache problem. This simple call should fix the problems
     [FBSDKAccessToken refreshCurrentAccessToken:nil];
@@ -181,7 +182,7 @@
         if (self.loginManager == nil) {
             self.loginManager = [[FBSDKLoginManager alloc] init];
         }
-        [self.loginManager logInWithReadPermissions:permissions fromViewController:self.viewController handler:loginHandler];
+        [self.loginManager logInWithReadPermissions:permissions fromViewController:[self topMostController] handler:loginHandler];
         return;
     }
 
@@ -252,7 +253,7 @@
         [FBSDKMessageDialog showWithContent:content delegate:self];
         return;
 
-    } else if ([method isEqualToString:@"share"] || [method isEqualToString:@"share_open_graph"] || [method isEqualToString:@"feed"]) {
+    } else if ([method isEqualToString:@"share"] || [method isEqualToString:@"feed"]) {
         // Create native params
         FBSDKShareLinkContent *content = [[FBSDKShareLinkContent alloc] init];
         content.contentURL = [NSURL URLWithString:params[@"href"]];
@@ -262,16 +263,61 @@
 
         self.dialogCallbackId = command.callbackId;
         FBSDKShareDialog *dialog = [[FBSDKShareDialog alloc] init];
-        dialog.fromViewController = self.viewController;
+        dialog.fromViewController = [self topMostController];
         dialog.shareContent = content;
         dialog.delegate = self;
         // Adopt native share sheets with the following line
         if (params[@"share_sheet"]) {
-            dialog.mode = FBSDKShareDialogModeShareSheet;
+        	dialog.mode = FBSDKShareDialogModeShareSheet;
+        } else if (params[@"share_feedBrowser"]) {
+        	dialog.mode = FBSDKShareDialogModeFeedBrowser;
+        } else if (params[@"share_native"]) {
+        	dialog.mode = FBSDKShareDialogModeNative;
+        } else if (params[@"share_feedWeb"]) {
+        	dialog.mode = FBSDKShareDialogModeFeedWeb;
         }
+
         [dialog show];
         return;
-    } else if ([method isEqualToString:@"apprequests"]) {
+    }
+    else if ( [method isEqualToString:@"share_open_graph"] ) {
+        if(!params[@"action"] || !params[@"object"]) {
+            NSLog(@"No action or object defined");
+            return;
+        }
+
+        //Get object JSON
+        NSError *jsonError;
+        NSData *objectData = [params[@"object"] dataUsingEncoding:NSUTF8StringEncoding];
+        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:objectData
+                                                             options:NSJSONReadingMutableContainers
+                                                               error:&jsonError];
+        
+        if(jsonError) {
+            NSLog(@"There was an error parsing your 'object' JSON string");
+        } else {
+            FBSDKShareOpenGraphObject *object = [FBSDKShareOpenGraphObject objectWithProperties:json];
+            FBSDKShareOpenGraphAction *action = [[FBSDKShareOpenGraphAction alloc] init];
+            action.actionType = params[@"action"];
+            if(!json[@"og:type"]) {
+                NSLog(@"No 'og:type' encountered in the object JSON. Please provide an Open Graph object type.");
+                return;
+            }
+            NSString *objectType = json[@"og:type"];
+            objectType = [objectType stringByReplacingOccurrencesOfString:@"."
+                                                               withString:@":"];
+            
+            [action setObject:object forKey:objectType];
+            FBSDKShareOpenGraphContent *content = [[FBSDKShareOpenGraphContent alloc] init];
+            content.action = action;
+            content.previewPropertyName = objectType;
+            [FBSDKShareDialog showFromViewController:self.topMostController
+                                         withContent:content
+                                            delegate:nil];
+        }
+        return;
+    }
+    else if ([method isEqualToString:@"apprequests"]) {
         FBSDKGameRequestDialog *dialog = [[FBSDKGameRequestDialog alloc] init];
         dialog.delegate = self;
         if (![dialog canShow]) {
@@ -301,7 +347,7 @@
         if (!filters) {
             content.filters = FBSDKGameRequestFilterNone;
         } else if ([filters isEqualToString:@"app_users"]) {
-            content.filters = FBSDKGameRequestFilterAppNonUsers;
+            content.filters = FBSDKGameRequestFilterAppUsers;
         } else if ([filters isEqualToString:@"app_non_users"]) {
             content.filters = FBSDKGameRequestFilterAppNonUsers;
         }
@@ -426,12 +472,24 @@
 
     FBSDKAppInviteDialog *dialog = [[FBSDKAppInviteDialog alloc] init];
     if ((url || picture) && [dialog canShow]) {
-        [FBSDKAppInviteDialog showFromViewController:self.viewController withContent:content delegate:self];
+        [FBSDKAppInviteDialog showFromViewController:[self topMostController] withContent:content delegate:self];
     } else {
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
         [self.commandDelegate sendPluginResult:result callbackId:self.dialogCallbackId];
     }
 
+}
+
+- (void) getDeferredApplink:(CDVInvokedUrlCommand *) command
+{
+    [FBSDKAppLinkUtility fetchDeferredAppLink:^(NSURL *url, NSError *error) {
+        if (error) {
+            NSLog(@"Received error while fetching deferred app link %@", error);
+        }
+        if (url) {
+            [[UIApplication sharedApplication] openURL:url];
+        }
+    }];
 }
 
 - (void) activateApp:(CDVInvokedUrlCommand *)command
@@ -471,11 +529,21 @@
 
     } else if (publishPermissionFound) {
         // Only publish permissions
-        [self.loginManager logInWithPublishPermissions:permissions fromViewController:self.viewController handler:handler];
+        [self.loginManager logInWithPublishPermissions:permissions fromViewController:[self topMostController] handler:handler];
     } else {
         // Only read permissions
-        [self.loginManager logInWithReadPermissions:permissions fromViewController:self.viewController handler:handler];
+        [self.loginManager logInWithReadPermissions:permissions fromViewController:[self topMostController] handler:handler];
     }
+}
+
+- (UIViewController*) topMostController {
+    UIViewController *topController = [UIApplication sharedApplication].keyWindow.rootViewController;
+
+    while (topController.presentedViewController) {
+        topController = topController.presentedViewController;
+    }
+
+    return topController;
 }
 
 - (NSDictionary *)responseObject {
@@ -644,7 +712,7 @@
                                      messageAsString:[NSString stringWithFormat:@"Error: %@", error.description]];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:self.dialogCallbackId];
     self.dialogCallbackId = nil;
-    
+
 }
 
 
