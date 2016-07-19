2df639d44265a62f71837387b8e92a0cda5d9f3e
Peter Vojtek <peter.vojtek@gmail.com>
2016-07-19 10:29:06 +0200

    #11: minor gui tweaks

--- www/js/controllers/friend-finder.js
+++ www/js/controllers/friend-finder.js
@@ -1,10 +1,12 @@
 angular.module('app.controllers').controller('friendFinderController', function ($scope, FriendFinder) {
   
   $scope.friends = []
+  $scope.noResults = false
   $scope.search = function(){
     $scope.showSpinner();
     FriendFinder.search().then(function(response){
       $scope.friends = response
+      $scope.noResults = $scope.friends.length == 0
       $scope.hideSpinner();
     })
   }
