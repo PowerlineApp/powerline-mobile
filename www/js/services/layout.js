5ce71415ce0cffba7a92f313b75a2f795cf53fe6
Peter Vojtek <peter.vojtek@gmail.com>
2016-08-03 11:26:55 +0200

    #158: main menu in two columns

--- www/js/services/layout.js
+++ www/js/services/layout.js
@@ -84,7 +84,7 @@ angular.module('app.services').factory('topBar',function ($location) {
       new MenuItem('Home', '/main', 'home-item'),
       new MenuItem('My Groups', '/groups', 'groups-item'),
       new MenuItem('My Influences', '/influences', 'influences-item'),
-      new MenuItem('My Representatives', '/representatives', 'representatives-item'),
+      new MenuItem('Representatives', '/representatives', 'representatives-item'),
       new MenuItem('Favorites', '/favorite-main', 'favorite-item'),
       new MenuItem('My Profile', '/profile', 'profile-item'),
       new MenuItem('Settings', '/settings', 'settings-item'),
