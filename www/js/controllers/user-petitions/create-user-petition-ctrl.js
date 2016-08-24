cbd99c44995f7bd94b40962b4d40d2098a220a13
Peter Vojtek <peter.vojtek@gmail.com>
2016-08-24 15:54:04 +0200

    #187: tweak create user petition ctrl

--- www/js/controllers/user-petitions/create-user-petition-ctrl.js
+++ www/js/controllers/user-petitions/create-user-petition-ctrl.js
@@ -1,40 +1,15 @@
 angular.module('app.controllers').controller('createUserPetitionCtrl',function ($scope, $stateParams, $document, userPetitions, groups, profile, homeCtrlParams, $rootScope) {
   $scope.groupID = $stateParams.groupID;
 
+  // TODO: show only groups for which user has not exceeded number of posts per month
   $scope.groups = groups.groupsJoinedByCurrentUser();
+  $scope.data = {}
 
-  $scope.expires_intervals = [
-    {
-      value: 1,
-      label: '1 day'
-    },
-    {
-      value: 3,
-      label: '3 days'
-    },
-    {
-      value: 7,
-      label: '7 days'
-    },
-    {
-      value: 30,
-      label: '30 days'
-    }
-  ];
-  $scope.data = {
-    is_outsiders_sign: false
-  };
-
-  if ($stateParams.groupID) {
-    $scope.data.group = _($scope.groups).find(function (item) {
-      return item.id === Number($stateParams.groupID);
-    });
-  } else {
+  if ($scope.groupID) 
+    $scope.data.group = groups.getGroup($scope.groupID)
+  else 
     $scope.data.openChoices = true;
-  }
-
-  $scope.user_expire_interval = $scope.expires_intervals[2];
-  $scope.petition_types = ['quorum', 'open letter', 'long petition'];
+  
   $scope.profile = profile.get();
 
   $scope.create = function(petitionForm) {
@@ -96,17 +71,4 @@ angular.module('app.controllers').controller('createUserPetitionCtrl',function (
     return $body.height() - 56 - 42;
   };
 
-  var start = new Date();
-  var month = start.getMonth();
-  start.setDate(0);
-  // petitions.loadByParams({user: session.user_id, start: start.toUTCString()}).then(function (collection) {
-  //   _($scope.groups).each(function (group) {
-  //     group.available = group.petition_per_month - collection.reduce(function (memo, petition) {
-  //       if (petition.get('group').id === group.id && petition.get('created_at').getMonth() === month) {
-  //         memo++;
-  //       }
-  //       return memo;
-  //     }, 0);
-  //   });
-  // });
 })
