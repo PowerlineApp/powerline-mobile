ad644e78c2aeb2ffe28fc061b2bf14e2605ed6d0
Peter Vojtek <peter.vojtek@gmail.com>
2016-09-02 08:21:09 +0200

    remove unused methods from activity-model

--- www/js/services/activity/activity-model.js
+++ www/js/services/activity/activity-model.js
@@ -68,9 +68,6 @@ angular.module('app.services').factory('ActivityModel',
           })
         }
       },
-      getResponsesToQuorum: function () {
-        return this.get('quorum') ? Math.max(this.get('quorum') - this.get('responses_count'), 1) : 1;
-      },
       getQuorumCompletedPercent: function () {
         return this.get('quorum') ?
           Math.min(this.get('responses_count') / this.get('quorum') * 100, 100) : 0;
@@ -113,61 +110,9 @@ angular.module('app.services').factory('ActivityModel',
 
         return 1;
       },
-
-      getPostID: function(){
-        return this.attributes.id;
-      },
-
-      saveProfileSetting: function(){
-
-       return $http({
-          method: 'POST',
-          url: serverConfig.url + '/api/profile/settings',
-          data: null
-        }).then(function(resp) {
-          return resp.data;
-        });
-      },
       
       hasLinkPreviewMetadata: function(){
         return !!this.get('metadata');
-      },
-
-      // makes sense only for micro-petition-long-petition and petition
-      canBeSigned: function(){
-        var notExpired = !this.isExpired()
-        if(this.isPollPetitionType()){
-          var notAnsweredOnBackend = this.get('answer') == null
-        } else {
-          var notAnsweredOnBackend = this.get('answers')&& this.get('answers').length == 0
-        }
-        var notAnsweredLocally = !this.get('answered')
-        var notOwnedByMe = !this.isOwn()
-        return notOwnedByMe && notExpired && notAnsweredOnBackend && notAnsweredLocally
-      },
-      canBeUnsigned: function(){
-        var notExpired = !this.isExpired()
-        var answeredOnBackend = this.get('answers') && this.get('answers').length > 0
-        var answeredLocally = this.get('answered')
-        return notExpired && (answeredOnBackend || answeredLocally)
-      },
-      markAsSigned: function(){
-        this.set('answered', true);
-      },
-      markAsUnsigned: function(){
-        this.set('answered', false);
-        this.set('answers', []);
-      },
-
-      userIsSubscribedToNotifications: function(){
-        var s2 = this.get('post') && this.get('post').is_subscribed
-        return s2
-      },
-      markAsUnsubscribed: function(){
-        this.set('post', {is_subscribed: false})
-      },
-      markAsSubscribed: function(){
-        this.set('post', {is_subscribed: true})
       }
     });
   })
