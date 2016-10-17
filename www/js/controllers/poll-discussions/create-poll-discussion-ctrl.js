661e32deb5d6f7f060fc821450eb1832e0132b9b
Peter Vojtek <peter.vojtek@gmail.com>
2016-10-17 09:27:13 +0200

    #343: submit create discussion

--- www/js/controllers/poll-discussions/create-poll-discussion-ctrl.js
+++ www/js/controllers/poll-discussions/create-poll-discussion-ctrl.js
@@ -1,4 +1,4 @@
-angular.module('app.controllers').controller('createPollDiscussionCtrl',function ($scope, $stateParams, $document, questions, groups, profile, homeCtrlParams, $rootScope, $q) {
+angular.module('app.controllers').controller('createPollDiscussionCtrl',function ($scope, $stateParams,questions, groups, profile, $http, serverConfig) {
   $scope.groupID = $stateParams.groupID;
   $scope.groups = groups.groupsJoinedByCurrentUser();
   $scope.data = {discussion_description: ''}
@@ -8,4 +8,35 @@ angular.module('app.controllers').controller('createPollDiscussionCtrl',function
   else 
     $scope.data.openChoices = true;
 
+  $scope.send = function(){
+    if($scope.data.discussion_description.length == 0){
+      alert('Discussion topic cannot be blank.')
+      return false
+    }
+
+    var groupID = $scope.data.group.id
+    var data = {subject: $scope.data.discussion_description,type: 'news'} 
+    var payload = JSON.stringify(data)
+    var headers = {headers: {'Content-Type': 'application/json'}}
+    $scope.showSpinner();
+    var createDiscussionRequest = $http.post(serverConfig.url + '/api/v2/groups/'+groupID+'/polls', payload, headers)
+    createDiscussionRequest.then(function(response){
+      var discussionID = response.data.id
+      questions.publishPoll(discussionID).then(function(){
+        $scope.hideSpinner();
+        $rootScope.showToast('Discussion successfully created!');
+        $rootScope.back();
+      }, function(error){
+        $scope.hideSpinner();
+        console.log(error)
+        alert('Failed to publish discussion due to: '+JSON.stringify(error.data))
+      })
+    }, function(error){
+      $scope.hideSpinner();
+      if(error.status == 403)
+        $scope.alert('You are not allowed to create discussion in this group')
+      else
+        $scope.alert('Error occured while creating Poll: '+JSON.stringify(error.data))
+    })
+  }
 })
