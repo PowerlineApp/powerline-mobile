f249bfb958deb2c697a85784169640f6199c209e
Peter Vojtek <peter.vojtek@gmail.com>
2016-10-20 07:53:47 +0200

    #355: add html

--- www/js/controllers/groups/manage-group-ctrl.js
+++ www/js/controllers/groups/manage-group-ctrl.js
@@ -2,6 +2,14 @@ angular.module('app.controllers').controller('manageGroupCtrl',function ($scope,
   var groupID = parseInt($stateParams.id)
   groups.loadAllDetails(groupID).then(function(){
     $scope.group = groups.get(groupID);
+    console.log($scope.group)
+
+    if($scope.group.membership_control == 'public')
+     $scope.data.membership_control = $scope.membershipControlOptions[0]
+    else if($scope.group.membership_control == 'approval')
+      $scope.data.membership_control = $scope.membershipControlOptions[1]
+    else if($scope.group.membership_control == 'passcode')
+      $scope.data.membership_control = $scope.membershipControlOptions[2]
   })  
 
   var expandedSection = null
@@ -12,4 +20,24 @@ angular.module('app.controllers').controller('manageGroupCtrl',function ($scope,
   $scope.isExpanded = function(sectionName){
     return expandedSection == sectionName
   }
+
+  $scope.dataIsAltered = function(){
+    if(!$scope.group)
+      return false
+
+    var mcChanged = $scope.group.membership_control != $scope.data.membership_control.value
+    return mcChanged
+  }
+
+  $scope.membershipControlSetToPasscode = function(){
+    return $scope.data.membership_control.value == 'passcode'
+  }
+
+  $scope.data = {membership_control: {}}
+  $scope.membershipControlOptions = [
+    {name: 'Public (Open to all)', value: 'public'},
+    {name: 'Approval (User is approved by group leader)', value: 'approval'},
+    {name: 'Passcode (User must provide correct passcode to enter)', value: 'passcode'}]
+
+
 })
