4b74086548b2430c1a8b4accdf1f75c0eb870558
Erik <eriklouis129@hotmail.com>
2016-11-14 18:40:59 +0100

    Humanizing Placeholder Text added "Create group" in nav menu

--- www/js/controllers/abstract-create-poll-ctrl.js
+++ www/js/controllers/abstract-create-poll-ctrl.js
@@ -117,4 +117,20 @@ angular.module('app.controllers').controller('abstractCreatePollCtrl',function (
      template: msg
    });
   }
+})
+
+.directive('focusMe', function($timeout) {
+  return {
+    link: function(scope, element, attrs) {
+      scope.$watch(attrs.focusMe, function(value) {
+        if(value === true) { 
+          console.log('value=',value);
+          $timeout(function() {
+            element[0].focus();
+            scope[attrs.focusMe] = false;
+          });
+        }
+      });
+    }
+  };
 });
--- www/js/directives/forms.js
+++ www/js/directives/forms.js
@@ -381,18 +381,4 @@ angular.module('app.directives').directive('iPlaceholder',function () {
         });
     }
   }
-}).directive('focusMe', function($timeout) {
-  return {
-    link: function(scope, element, attrs) {
-      scope.$watch(attrs.focusMe, function(value) {
-        if(value === true) { 
-          console.log('value=',value);
-          $timeout(function() {
-            element[0].focus();
-            scope[attrs.focusMe] = false;
-          });
-        }
-      });
-    }
-  };
 });
