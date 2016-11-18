c2269f44e9ecb1d9bbe92d174af254059219f12a
Peter Vojtek <peter.vojtek@gmail.com>
2016-11-18 08:04:30 +0100

    #338: add image attachments

--- www/js/services/attachments.js
+++ www/js/services/attachments.js
@@ -58,3 +58,100 @@ angular.module('app.directives').directive('showAttachments', function (attachme
       }
     };
 })
+
+angular.module('app.directives').directive('addAttachments', function (attachments, $ionicPopup) {
+    return {
+      restrict: 'E',
+      scope: {},
+      templateUrl: 'templates/attachments/add_attachments.html',
+      controller: function ($scope) {
+        $scope.attachments = {text: [], images: [], videos: []}
+        $scope.displayAttachmentsForm = true
+        var MAX_ATTACHMENT_ITEMS = 3
+
+        var canAddMoreContent = function(){
+          var a = $scope.attachments
+          return (a.text.length + a.images.length + a.videos.length) < MAX_ATTACHMENT_ITEMS
+        } 
+
+        $scope.addText = function(){
+          $scope.attachments.text = ['']
+        }
+
+        $scope.removeText = function(){
+          var confirmPopup = $ionicPopup.confirm({
+            title: 'Remove text?',
+            cssClass: 'popup-by-ionic',
+            buttons: [{
+              text: 'No'
+            }, {
+              text: 'Yes',
+              type: 'button-assertive',
+              onTap: function(e) {
+                return (true)
+              }
+            }]
+          });
+          confirmPopup.then(function(res) {
+            if (res) 
+              $scope.attachments.text = []
+          });
+          
+        }
+
+        $scope.addTextButtonVisible = function(){
+          return canAddMoreContent() && $scope.attachments.text.length == 0
+        }
+
+        $scope.textAttachmentVisible = function(){
+          return $scope.attachments.text.length > 0
+        }
+
+        $scope.addImageButtonVisible = function(){
+          return canAddMoreContent()
+        }
+
+        $scope.addImage = function(){
+          if(!ionic.Platform.isAndroid() && !ionic.Platform.isIOS()){
+            $scope.attachments.images.push('images/v2/logo.png')
+          } else {
+            navigator.camera.getPicture(function(img) {
+              var imageB64URI = 'data:image/jpeg;base64,' + img
+              $scope.attachments.images.push(imageB64URI)
+              $scope.$apply();
+            }, function(e) {
+              //  image selection in album canceled by user
+            }, {
+              correctOrientation: true,
+              targetWidth: 1280,
+              targetHeight: 1280,
+              destinationType: navigator.camera.DestinationType.DATA_URL,
+              sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+            });
+          }
+
+        }
+
+        $scope.removeImage = function(imagePos){
+          var confirmPopup = $ionicPopup.confirm({
+            title: 'Remove image?',
+            cssClass: 'popup-by-ionic',
+            buttons: [{
+              text: 'No'
+            }, {
+              text: 'Yes',
+              type: 'button-assertive',
+              onTap: function(e) {
+                return (true)
+              }
+            }]
+          });
+          confirmPopup.then(function(res) {
+            if (res) 
+              $scope.attachments.images.splice(parseInt(imagePos), 1)
+          });
+        }
+
+      }
+    }
+})
