eae921feb42c36a66393fb1f116a08f2fb868870
Erik <erlendfrendsen@erlend.local>
2016-11-21 18:07:49 +0100

    #378: New Event - Pre-fill RSVP options

--- www/js/controllers/poll-events/create-poll-event-ctrl.js
+++ www/js/controllers/poll-events/create-poll-event-ctrl.js
@@ -12,7 +12,7 @@ angular.module('app.controllers').controller('createPollEventCtrl',function ($sc
   $scope.data.title = ''
   $scope.data.desc = ''
 
-  $scope.answers = [{desc: '', placeholder: 'Yes, I can make it'}, {desc: '', placeholder: 'No, I can\'t make it'}]
+  $scope.answers = [{desc: 'Yes, I can make it', placeholder: 'Yes, I can make it'}, {desc: 'No, I can\'t make it', placeholder: 'No, I can\'t make it'}]
   $scope.removeAnswer = function(index){
     if($scope.answers.length <= 2)
       $scope.validationAlert('You must provide at least two answers.')
