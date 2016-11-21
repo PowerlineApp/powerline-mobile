14bf417ceb9a157c005b9bbde2c987e65c946dfe
Peter Vojtek <peter.vojtek@gmail.com>
2016-11-21 13:12:08 +0100

    remove forgotten console.log

--- www/js/directives/forms.js
+++ www/js/directives/forms.js
@@ -359,7 +359,6 @@ angular.module('app.directives').directive('iPlaceholder',function () {
     link: function(scope, element, attrs) {
       scope.$watch(attrs.focusMe, function(value) {
         if(value === true) { 
-          console.log('value=',value);
           $timeout(function() {
             element[0].focus();
             scope[attrs.focusMe] = false;
