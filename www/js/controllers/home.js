ed5690542d6a127a5b85828ba50d88954d2c6322
Peter Vojtek <peter.vojtek@gmail.com>
2016-11-28 09:38:39 +0100

    minor

--- www/js/controllers/home.js
+++ www/js/controllers/home.js
@@ -341,7 +341,7 @@ angular.module('app.controllers').directive('iActivity', function ($rootScope, q
         }
       };
 
-      // if($scope.activity.get('entity').id == 239)
+      // if($scope.activity.get('entity').id == 491)
       //     console.log(JSON.stringify($scope.activity))
       // else return
 
