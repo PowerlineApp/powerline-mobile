f003e5d3a1c4b02d30fb1775ab0b7f9876f2c48e
devotebest <david.mobile527@gmail.com>
2017-01-30 12:56:39 +0800

    #410 Add basic report

--- www/js/directives/group-bank-account-form.js
+++ www/js/directives/group-bank-account-form.js
@@ -4,7 +4,8 @@ angular.module('app.directives').directive('groupBankAccountForm', function () {
       scope: {
         cancel: '&',
         completed: '&',
-        group: '='
+        group: '=',
+        profile: '='
       },
       templateUrl: 'templates/groups/manage-group/add-group-bank-account.html',
       controller: function ($scope, $rootScope, cards) {
@@ -53,17 +54,25 @@ angular.module('app.directives').directive('groupBankAccountForm', function () {
         }
 
         var prefillForm = function(){
+          if($scope.profile && $scope.profile.birth) {
+            var dob = new Date($scope.profile.birth);
+          }
           $scope.data.powerline = {
-            first_name: '',
-            last_name: '',
+            first_name: $scope.profile.first_name,
+            last_name: $scope.profile.last_name,
             ssn_last_4: '',
-            address_line1: $scope.group.official_address,
-            address_line2: '',
-            city: $scope.group.official_city,
-            state: $scope.group.official_state,
-            postal_code: ''
+            address_line1: $scope.profile.address1,
+            address_line2: $scope.profile.address2,
+            city: $scope.profile.city,
+            state: $scope.profile.state,
+            postal_code: $scope.profile.zip,
+            dob: dob
           }
         }
+
+        $scope.$watch('profile', function (val) {
+            prefillForm()
+        });
 
         $scope.$watch('group', function (val) {
             prefillForm()
